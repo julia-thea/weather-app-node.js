fcc14596698c226fdf6732594fa08bf9bbb81979
Julia Thea <yuliya.boyadjan@gmail.com>
2020-07-13 18:27:39 -0500

    adding requests

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('postman-request')
 
-setTimeout(() => {
-  console.log('2 Second Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=7db4a9628da1809e8f221d2e4a95a84d&query=37.8267,-122.4233'
 
-setTimeout(() => {
-  console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body)
+  console.log(data.current)
+})
