4f41306d3f9fe0e3a4e9c492163a30e0a3fb95ae
Julia Thea <yuliya.boyadjan@gmail.com>
2020-08-17 10:44:44 -0500

    finished forecast" gp

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const geocode = require('./utils/geocode')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-geocode('Minsk', (error, data) => {
-  console.log('Error: ', error)
-  console.log('Data: ', data)
-})
+// geocode('Minsk', (error, data) => {
+//   console.log('Error: ', error)
+//   console.log('Data: ', data)
+// })
+
+forecast('Minsk', (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+}) 
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,5 +1,18 @@
-const forecast = () = {
+const request = require('postman-request')
 
+const forecast = (address, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=7db4a9628da1809e8f221d2e4a95a84d&query=' + encodeURIComponent(address) + '&units=f'
+  request( {url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined)
+    } else if (response.body.error) {
+      callback('Unable to display weather stats for chosen location. Try something else', undefined)
+    } else {
+      callback(undefined, {
+        weather: response.body.current.temperature
+      })
+    }
+  })
 }
 
-module.exports = forecast;
+module.exports = forecast
