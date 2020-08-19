7171ef583850349829bb912855a5cbc3efcd5752
Julia Thea <yuliya.boyadjan@gmail.com>
2020-08-19 13:15:22 -0500

    Updated to accept command line argument

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geocode('Minsk', (error, data) => {
-  console.log('Error: ', error)
-  console.log('Data: ', data)
-  forecast(data.location, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+  if(error) {
+    return console.log(error)
+  } 
+
+  forecast(data.location, (error, forecastData) => {
+    if (error) {
+      return console.log(error)
+    }
+
+
+    console.log(data.location)
+    console.log(forecastData)
   }) 
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,16 +1,18 @@
 const request = require('postman-request')
 
+
 const forecast = (address, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=7db4a9628da1809e8f221d2e4a95a84d&query=' + encodeURIComponent(address) + '&units=f'
+  const args = process.argv;
+  const url = 'http://api.weatherstack.com/current?access_key=7db4a9628da1809e8f221d2e4a95a84d&query=' + encodeURIComponent(args[2]) + '&units=f'
   request( {url: url, json: true }, (error, response) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
     } else if (response.body.error) {
       callback('Unable to display weather stats for chosen location. Try something else', undefined)
     } else {
-      callback(undefined, {
-        weather: `Weather for ${response.body.location.name} is ${response.body.current.temperature}`
-      })
+      callback(undefined,
+        `Weather for ${response.body.location.name} is ${response.body.current.temperature}, ${response.body.current.weather_descriptions}`
+      )
     }
   })
 }
