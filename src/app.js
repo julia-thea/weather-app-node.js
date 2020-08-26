24c7fb4f25c768d7ffe475730de0f17368741539
Julia Thea <yuliya.boyadjan@gmail.com>
2020-08-26 16:04:40 -0500

    mods for heroku deployment

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ const { send } = require('process')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast')
 
-// Define paths for Express config
 const app = express()
+const port = process.env.PORT || 3000 //Points to port or defaults to local port 3000
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -96,6 +98,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
 })
