c5dd816b70c3bbfdc934f42b836cad3364e5dd2e
Julia Thea <yuliya.boyadjan@gmail.com>
2020-08-27 12:57:47 -0500

    finished weather API

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,21 @@
-console.log("Hello js file")
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
-})
-
-fetch('http://api.weatherstack.com/current?access_key=7db4a9628da1809e8f221d2e4a95a84d&query=texas').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location.name, data.current.temperature, data.current.weather_descriptions)
-    }
-  })
-})
+console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
-weatherForm.addEventListener('submit', (event) => {
-  event.preventDefault()
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-  const location = search.value
-  console.log(location)
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
 })
