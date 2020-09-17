65f2c4d0a6ccb56ffe1071262bf2532db4df33e5
Julia Thea <yuliya.boyadjan@gmail.com>
2020-09-17 16:13:45 -0500

    async await practice

--- practice/playground/9-async-await.js
+++ practice/playground/9-async-await.js
@@ -1,5 +1,20 @@
-const doWork = async () => {
-
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 2000)
+  })
 }
 
-console.log(doWork())
+const doWork = async () => {
+  const sum = await add(1, 99)
+  const sum2 = await add(sum, 50)
+  const sum3 = await add(sum2, 3)
+  return sum3
+}
+
+doWork().then((result) => {
+  console.log('result ', result)
+}).catch((e) => {
+  console.log('e ', e)
+})
